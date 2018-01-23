5e17236a853963f12bc00b2a3234841eab8a10ea
IgarashiToure <2545369032@qq.com>
2018-01-23 11:30:07 +0800

    add xbot_take_depth_picture.py

--- frontend/blockly/generators/python/scripts/xbot_sensor/xbot_take_depth_picture.py
+++ frontend/blockly/generators/python/scripts/xbot_sensor/xbot_take_depth_picture.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-import rospy,rospkg
-import time
+import sys
+import rospy
 import subprocess
+import rosnode
 import numpy as np
 import cv2
-import sys
+import time
 import os
-from sensor_msgs.msg import PointCloud2
+import rospkg	
+from sensor_msgs.msg import Image
 
 print("take a depth picture")
 
@@ -15,26 +17,37 @@ def take_depth_picture():
   command = "rostopic list"
   process = subprocess.Popen(command,shell = True, stdout = subprocess.PIPE)
   topic = process.communicate(input = None)
-  if "/camera/depth/points" in topic[0]:
-    time_format = "%Y-%m-%d_%X"
+  if "/camera/depth/image_raw" in topic[0]:
+    time_format = "%Y-%m-%d_%X.jpeg"
     timestr = time.strftime(time_format,time.localtime())
     
-    print("take start")
-    msg_image = rospy.wait_for_message("camera/depth/points", PointCloud2, timeout = 7)
+    msg_image = rospy.wait_for_message("camera/depth/image_raw", Image, timeout = 7)
     
-    np_arr = np.fromstring(msg_image,np.uint8)
-    image_np = cv2.imdecode(np_arr, 1)
+    np_arr = np.fromstring(msg_image.data, np.uint8)
+ #   print(np_arr,"type: ",type(np_arr))
+ #   image_np = cv2.imdecode(np_arr, 1) 
 
+    nrows, ncols = msg_image.height,msg_image.width
+
+    print("encoding:",msg_image.encoding,"is_bigendian:",msg_image.is_bigendian,
+"step:",msg_image.step)
+    print("row:",ncols,"col:",nrows,"r*c:",ncols*nrows,"len:",len(msg_image.data))
+
+    nparr = np.fromstring(np_arr, dtype=np.uint8).reshape(nrows, ncols, 4)
+    image_np = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)#1
+    
+    #find path
     rospack = rospkg.RosPack()
-    images_path = rospack.get_path("robot_blockly") + "/frontend/pages/depth_images"
-
-    cv2.imwrite(images_path + "depthImage_" + timestr,image_np)
-
-    files = len(os.listdir(images_path))
-
-    if files > 7:
-      os.system("find " +images_path + " -name '*.png' | xargs ls -t | tail -n 1 | xargs rm")
+    images_path = rospack.get_path('robot_blockly') + '/frontend/pages/depth_images/'
+    
+    #write picture to path
+    cv2.imwrite(images_path+ 'depthimage_' + timestr, image_np, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+    
+    #amount of files in /frontend/images/ folder
+    files = len(os.listdir(images_path))   
+    if files > 7 : #allow 5 images max  
+        os.system("find "+images_path+" -name '*.png' | xargs ls -t | tail -n 1 | xargs rm")#remove oldest image
   else:
-    print("topic:\"/camera/depth/points\" is not found!")  
+    print("topic:\"/camera/depth/image_raw\" is not found!")  
 
 take_depth_picture()
